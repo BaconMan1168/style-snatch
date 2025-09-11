5c1093f8b17ce328b945a0ad4a7db2cba185384c
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-09-11 12:57:20 +0800

    Added react router SPA functionality to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,6 @@ function App({fonts, colors}) {
       </Link>
 
       <Outlet context={{fonts, colors}}></Outlet>
-      <h2>Colors:</h2>
-      <ul>
-        {colorsAndFonts[0] && colors.map((c, i) => <li key={i}>{c}</li>)}
-      </ul>
-      <h2>Fonts:</h2>
-      <ul>
-        {colorsAndFonts[1] && fonts.map((f, i) => <li key={i}>{f.font} ({f.size})</li>)}
-      </ul>
     </main>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import ColorPage from "./ColorPage.jsx"
+import FontPage from "./FontPage.jsx"
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
 
 chrome.storage.local.get(null, data => {
+  const router = createBrowserRouter([
+    {
+      element: <App {...data} />,
+      children: [
+        { path: "/colors", element: <ColorPage /> },
+        { path: "/fonts", element: <FontPage /> }
+      ]
+    }
+  ])
+
   createRoot(document.getElementById('root')).render(
     <StrictMode>
-      <App {...data} />
+      <RouterProvider router={router} />
     </StrictMode>
   )
 })
